d6af99ee009b0147c2296548f8efa8c1e49ccaec
Alejandro Melo Garcia <ale.garcia3003@gmail.com>
2021-12-08 00:10:18 -0600

    first commit

---
+++ spec/reservamos/kueski_pay_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Reservamos::KueskiPay do
+  it "has a version number" do
+    expect(Reservamos::KueskiPay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
